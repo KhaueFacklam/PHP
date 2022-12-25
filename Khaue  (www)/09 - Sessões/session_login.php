1e16d1d9d2264aa8c3f370c508b175cf5c6307ae
Khaue Tavares <Khauefacklam@gmail.com>
2022-12-25 20:40:46 -0300

    -

--- Khaue  (www)/09 - Sessões/session_login.php
+++ Khaue  (www)/09 - Sessões/session_login.php
@@ -1,15 +1,41 @@
 <?php
-	// Modifica o tempo de expiração da sessão (padrão são 180 minutos)	
-	//session_cache_expire(1);
 	
 	// Inicia a sessão
-	session_start();
+	session_start(); /
+	session_cache_expire(1); // Modifica o tempo de expiração (padrão são 180 minutos)
 
-	// Cria as chaves e seus valores
+	// Cria chaves e adiciona valores
 	$_SESSION['usuario'] = 'sergio';
 	$_SESSION['senha']   = 'sergio123';
 	
-	// Mostra uma frase na tela
+	// Exibi um valor salvo
 	echo 'Olá ' . $_SESSION['usuario'];
+	
+	// Deleta uma variável
+	unset($_SESSION['usuario'])
+
+	// Destrói toda sessão
+	session_destroy();
+
+	// Verifica se a sessão não existe (isset verifica se uma variável foi iniciada)
+	if(!isset($_SESSION['usuario']) && !isset($_SESSION['senha'])) {
+		exit('Não há sessão ativa'); // mata o scrip e envia uma mensagem
+	}
+
+	// Verifica se a sessão do usuário está vazia
+	if(empty($_SESSION['usuario']) && empty($_SESSION['senha'])) {
+		exit('Sessão terminada! Faça o login novamente'); // mata o scrip e envia uma mensagem
+	}
+
+
+
+	
+
+
+	// 
+
+
+
+
 ?>
 
